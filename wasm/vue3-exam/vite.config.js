21873a78ff89b09dc729d2a90040c420cedcf3d0
Zhenchun Li <zhenchun.li@kunlun-inc.com>
2026-01-09 18:59:46 +0800

    vue

--- wasm/vue3-exam/vite.config.js
+++ wasm/vue3-exam/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    wasm(),
-    topLevelAwait()
   ],
   optimizeDeps: {
     exclude: ['soundtouch']
